d8a0fa276bccf72f984d1ac145872520bb13acda
peterjc123 <peter_jiachen@163.com>
2024-04-07 07:27:49 +0000

    fix train for qwen2

--- functionary/train/train.py
+++ functionary/train/train.py
@@ -16,7 +16,7 @@ from transformers import AutoConfig, AutoTokenizer, Trainer
 
 #  sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from functionary.prompt_template import PromptTemplate, get_prompt_template_by_version
-from functionary.train.custom_datasets import read_dataset
+from functionary.train.custom_datasets import read_dataset, SimpleCollator
 
 LOCAL_RANK = int(os.getenv("LOCAL_RANK", "0"))
 
@@ -102,40 +102,54 @@ def initialize_tokenizer(
     else:
         padding_side = "right"
 
-    # note that must set legacy=True, read more: https://github.com/huggingface/transformers/issues/25176
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_name_or_path,
-        cache_dir=cache_dir,
-        model_max_length=model_max_length,
-        padding_side=padding_side,
-        legacy=True,
-    )
+    if model.config.model_type == 'qwen2':
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path,
+            cache_dir=cache_dir,
+            model_max_length=model_max_length,
+            padding_side="right",
+        )
+    else:
+        # note that must set legacy=True, read more: https://github.com/huggingface/transformers/issues/25176
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_name_or_path,
+            cache_dir=cache_dir,
+            model_max_length=model_max_length,
+            padding_side=padding_side,
+            legacy=True,
+        )
 
     # Add special tokens
-    tokenizer.pad_token = tokenizer.eos_token
-    added_tokens = prompt_template.get_additional_tokens()
-    special_tokens = {"additional_special_tokens": added_tokens}
-    num_new_tokens = tokenizer.add_special_tokens(special_tokens)
+    if tokenizer.pad_token is None:
+        tokenizer.pad_token = tokenizer.unk_token
 
-    # add chat_template for tokenizer
-    tokenizer.chat_template = prompt_template.get_chat_template_jinja()
-    print("tokenizer: ", tokenizer)
+    if model.config.model_type == 'qwen2':
+        tokenizer.pad_token = "<|endoftext|>"
 
-    # Resize embedding
-    model.resize_token_embeddings(len(tokenizer))
-    if num_new_tokens > 0:
-        input_embeddings = model.get_input_embeddings().weight.data
-        output_embeddings = model.get_output_embeddings().weight.data
+    # tokenizer.pad_token = tokenizer.eos_token
+    # added_tokens = prompt_template.get_additional_tokens()
+    # special_tokens = {"additional_special_tokens": added_tokens}
+    # num_new_tokens = tokenizer.add_special_tokens(special_tokens)
 
-        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(
-            dim=0, keepdim=True
-        )
-        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(
-            dim=0, keepdim=True
-        )
+    # # add chat_template for tokenizer
+    # tokenizer.chat_template = prompt_template.get_chat_template_jinja()
+    # print("tokenizer: ", tokenizer)
 
-        input_embeddings[-num_new_tokens:] = input_embeddings_avg
-        output_embeddings[-num_new_tokens:] = output_embeddings_avg
+    # # Resize embedding
+    # model.resize_token_embeddings(len(tokenizer))
+    # if num_new_tokens > 0:
+    #     input_embeddings = model.get_input_embeddings().weight.data
+    #     output_embeddings = model.get_output_embeddings().weight.data
+
+    #     input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(
+    #         dim=0, keepdim=True
+    #     )
+    #     output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(
+    #         dim=0, keepdim=True
+    #     )
+
+    #     input_embeddings[-num_new_tokens:] = input_embeddings_avg
+    #     output_embeddings[-num_new_tokens:] = output_embeddings_avg
 
     return tokenizer
 
@@ -164,7 +178,7 @@ def extract_unmasked_chunks(labels: List[int], masked_value) -> List[List[int]]:
 
 
 def print_some_examples(ds, tokenizer):
-    data_loader = DataLoader(ds, batch_size=3)
+    data_loader = DataLoader(ds, batch_size=1)
     count = 0
     for batch in data_loader:
         if count == 0:
@@ -204,13 +218,13 @@ def train():
         model_args.model_name_or_path,
         cache_dir=training_args.cache_dir,
     )
-    orig_ctx_len = getattr(config, "max_position_embeddings", None)
-    if orig_ctx_len and training_args.model_max_length > orig_ctx_len:
-        scaling_factor = float(math.ceil(training_args.model_max_length / orig_ctx_len))
-        config.rope_scaling = {"type": "linear", "factor": scaling_factor}
-        print_rank0("Rope scaling enabled")
+    # orig_ctx_len = getattr(config, "max_position_embeddings", None)
+    # if orig_ctx_len and training_args.model_max_length > orig_ctx_len:
+    #     scaling_factor = float(math.ceil(training_args.model_max_length / orig_ctx_len))
+    #     config.rope_scaling = {"type": "linear", "factor": scaling_factor}
+    #     print_rank0("Rope scaling enabled")
     config.use_cache = False
-    config.sliding_window = training_args.model_max_length
+    # config.sliding_window = training_args.model_max_length
 
     if data_args.packing:
         print("Packing=True, using monkey-patched")
@@ -239,8 +253,15 @@ def train():
             )
 
             monkey_patch_packing_mixtral()
+        elif "qwen" in config_type:
+            print_rank0("using Monkey-patched Qwen2")
+            from functionary.train.packing.monkey_patch_packing import (
+                monkey_patch_packing_qwen2,
+            )
+
+            monkey_patch_packing_qwen2()
         else:
-            print("packing only supports models: Mistral, Llama, Mixtral")
+            print("packing only supports models: Mistral, Llama, Mixtral, Qwen2")
             sys.exit(1)
 
     compute_dtype = (
@@ -262,10 +283,10 @@ def train():
         setattr(model.config, "output_router_logits", True)
         print_rank0("Activate computing load balancing loss")
 
-    print_rank0("Prompt template to use: ", training_args.prompt_template_version)
-    prompt_template = get_prompt_template_by_version(
-        training_args.prompt_template_version
-    )
+    # print_rank0("Prompt template to use: ", training_args.prompt_template_version)
+    # prompt_template = get_prompt_template_by_version(
+    #     training_args.prompt_template_version
+    # )
 
     tokenizer = initialize_tokenizer(
         model=model,
@@ -279,18 +300,18 @@ def train():
         if not os.path.exists(training_args.output_dir):
             os.mkdir(training_args.output_dir)
 
-        tokenizer_folder = os.path.join(training_args.output_dir, "tokenizer")
-        if not os.path.exists(tokenizer_folder):
-            os.mkdir(tokenizer_folder)
-        # Save tokenizer
-        tokenizer.save_pretrained(tokenizer_folder)
+        # tokenizer_folder = os.path.join(training_args.output_dir, "tokenizer")
+        # if not os.path.exists(tokenizer_folder):
+        #     os.mkdir(tokenizer_folder)
+        # # Save tokenizer
+        # tokenizer.save_pretrained(tokenizer_folder)
 
     # get id of added tokens to compute the accuracy of predicing the token
-    id2token = {
-        tokenizer.encode(token)[-1]: token
-        for token in prompt_template.get_additional_tokens()
-    }
-    print_rank0("id to tokens: ", id2token)
+    # id2token = {
+    #     tokenizer.encode(token)[-1]: token
+    #     for token in prompt_template.get_additional_tokens()
+    # }
+    # print_rank0("id to tokens: ", id2token)
 
     assert data_args.train_data_path is not None, "Please provide a training data file."
 
@@ -320,7 +341,7 @@ def train():
         loss_fn = CrossEntropyLoss(reduction="none")
         shift_logits = logits[..., :-1, :].contiguous()
         shift_labels = labels[..., 1:].contiguous()
-        shift_logits = shift_logits.view(-1, len(tokenizer))
+        shift_logits = shift_logits.view(-1, shift_logits.size(-1))
         shift_labels = shift_labels.view(-1)
         loss = loss_fn(shift_logits, shift_labels)
         loss = torch.mean(loss.view(logits.shape[0], -1), dim=-1)
@@ -334,7 +355,6 @@ def train():
 
         acc_count = 0
         total_num = 0
-        dic = {token_id: {"acc": 0, "total": 0} for token_id in id2token}
         for pred, label in zip(
             predictions.flatten().tolist(), labels.flatten().tolist()
         ):
@@ -342,27 +362,17 @@ def train():
                 if label == pred:
                     acc_count += 1
                 total_num += 1
-            if label in dic:
-                dic[label]["total"] += 1
-                if label == pred:
-                    dic[label]["acc"] += 1
 
         # Calculate perplexity
         loss = eval_preds.predictions[1].tolist()
         loss = sum(loss) / len(loss)
         perplexity = math.exp(loss)
 
-        metrics = {"accuracy": acc_count / total_num, "perplexity": perplexity}
-        for token_id in dic:
-            token = id2token[token_id]
-            total_num = dic[token_id]["total"]
-            acc = -1
-            if total_num > 0:
-                acc = dic[token_id]["acc"] / total_num
-            metrics[f"accuracy_{token}"] = acc
-            metrics[f"accuracy_total_num_{token}"] = total_num
+        return {"accuracy": acc_count / total_num, "perplexity": perplexity}
 
-        return metrics
+    data_collator = None
+    if not data_args.packing:
+        data_collator = SimpleCollator(tokenizer)
 
     if training_args.do_eval:
         trainer = Trainer(
@@ -373,6 +383,7 @@ def train():
             eval_dataset=eval_dataset,
             compute_metrics=compute_metrics,
             preprocess_logits_for_metrics=preprocess_logits_for_metrics,
+            data_collator=data_collator,
         )
     else:
         trainer = Trainer(
@@ -380,6 +391,7 @@ def train():
             tokenizer=tokenizer,
             args=training_args,
             train_dataset=train_dataset,
+            data_collator=data_collator,
         )
 
     if list(pathlib.Path(training_args.output_dir).glob("checkpoint-*")):
